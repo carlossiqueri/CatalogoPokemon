a11d369f8548031c36b0fe948741367031c22ccc
Carlos Dias <carlsdias.dev@gmail.com>
2022-07-27 18:20:00 -0300

    Projeto Finalizado

---
+++ js/script.js
@@ -0,0 +1,53 @@
+var formulario = document.querySelector('form')
+
+formulario.addEventListener('submit', function(e){
+    
+    // Evita a página de executar um refresh
+    e.preventDefault()
+
+    // Url da pesquisa
+    let urlForm = 'https://pokeapi.co/api/v2/pokemon/';
+
+    // Valor do input Name
+    let nome = document.getElementById('name')
+
+    // Concatena a url com o inputname
+    urlForm = urlForm +this.name.value 
+
+    // Transforma os valores em minúsculas
+    urlForm = urlForm.toLocaleLowerCase()
+
+    // ID content
+    let resposta = document.getElementById('content')
+
+    //ID imgPokemon
+    let imagem = document.getElementById('imgPokemon')
+
+    // Resposta em HTML 
+    let html = ''
+
+    fetch(urlForm)
+        .then(resposta => resposta.json())
+        .then(function(data){
+            console.log(data)
+            html = 'Nome: ' + maiuscula(data.name) + '<br>'
+            html = html + 'Type: ' + maiuscula(data.types[0].type.name)
+            resposta.innerHTML = html
+
+            imagem.innerHTML = "<img src= '" + data.sprites.front_default +"'><img src= '" + data.sprites.back_default +"'>"
+        })
+        .catch(function(err){
+            if(err == 'SyntaxError: Unexpected token N in JSON at position 0'){
+                html = 'Pokémon não encontrado!'
+                imagem.innerHTML = null
+            } else {
+                html = 'Erro:' + err
+            }
+            resposta.innerHTML = html
+        })
+
+})
+
+function maiuscula(val){
+    return val[0].toUpperCase() + val.substr(1)
+}
